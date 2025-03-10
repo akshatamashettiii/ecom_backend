774d2ab275299633de3e7feaf17ae330081749fb
akshatamashettiii <kushigbharamagoudr@klebcahubli.in>
2025-03-10 16:11:11 +0530

    third

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const express=require(`express`)
 const connectDB=require("./config/db")
 const app=express()
 
+const allowedOrigins=["ecom-frontend-pla94cva6-kushalagbs-projects.vercel.app","ecom-frontend-eight-fawn.vercel.app"]
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin: function (origin, callback) {
+          if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+          } else {
+            callback(new Error("Not allowed by CORS"));
+          }
+        },
+        credentials: true, // Allows cookies and authentication headers if needed
+      }
+))
 connectDB()
 app.use("/auth",require("./routes/authRoutes"))
 app.use("/cart",require("./routes/cartRoutes"))
